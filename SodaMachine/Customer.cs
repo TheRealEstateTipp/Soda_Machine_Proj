1853b364c37af0ac05ee8d1fc5d88c6bd3a4d177
unknown <mstiff1983@gmail.com>
2020-05-19 17:05:45 -0400

    I have added a SelectPayment method and a SelectSoda method to the Customer class to allow them choose. I have also added a RemoveCoin method to remove coin from the wallet after they chose and a RemoveSoda method to remove the soda from the inventory of the SodaMachine

--- SodaMachine/Customer.cs
+++ SodaMachine/Customer.cs
@@ -10,11 +10,96 @@ namespace SodaMachine
     {
         public Wallet wallet;
         public BackPack backpack;
+        public List<Coin> payment;
+        public SodaMachine sodaMachine;
 
         public Customer()
         {
             wallet = new Wallet();
             backpack = new BackPack();
+            payment = new List<Coin>();
+            sodaMachine = new SodaMachine();
+
+        }
+        public void GetPayment()
+        {
+            //loop this stuff
+
+         
+            Console.WriteLine("Please Enter Your Payment");
+            string coin = Console.ReadLine();
+
+            switch (coin)
+            {
+                case "Quarter":
+                    Console.WriteLine("You have selected a Quarter");
+                    RemoveCoin("Quarter");
+                    break;
+                case "Dime":
+                    Console.WriteLine("You have selected a Dime");
+                    RemoveCoin("Dime");
+                    break;
+                case "Nickle":
+                    Console.WriteLine("You have selected a Nickle");
+                    RemoveCoin("Nickle");
+                    break;
+                case "Penny":
+                    Console.WriteLine("You have selected a Penny");
+                    RemoveCoin("Penny");
+                    break;
+                default:
+                    Console.WriteLine("This Coin is not available");
+                    break;
+
+            }
+        }
+        public void RemoveCoin(string coinType)
+        {
+            for (int i = 0; i < wallet.coins.Count; i++)
+            {
+                if (wallet.coins[i].name == coinType)
+                {
+                    payment.Add(wallet.coins[i]);
+                    wallet.coins.RemoveAt(i);
+                    break;
+                }
+            }
+        }
+
+        public void SelectSoda()
+        {
+            Console.WriteLine("Please Select Your Desired Soda");
+            string soda = Console.ReadLine();
+
+            switch (soda)
+            {
+                case "Cola":
+                    Console.WriteLine("You have selected a Cola");
+                    RemoveCoin("Cola");
+                    break;
+                case "Orange Soda":
+                    Console.WriteLine("You have selected a Orange Soda");
+                    RemoveCoin("Orange Soda");
+                    break;
+                case "Root Beer":
+                    Console.WriteLine("You have selected a Root Beer");
+                    RemoveCoin("Root Beer");
+                    break;
+                default:
+                    Console.WriteLine("This selection is not available");
+                    break;
+            }
+        }
+        public void RemoveSoda(string sodaType)
+        {
+            for (int i = 0; i < sodaMachine.inventory.Count; i++)
+            {
+                if (sodaMachine.inventory[i].name == sodaType)
+                {
+                    sodaMachine.inventory.RemoveAt(i);
+                    break;
+                }
+            }
 
         }
     }
