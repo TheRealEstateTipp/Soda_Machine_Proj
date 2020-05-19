010a30a02452c27d934aa74a0791483399197960
unknown <mstiff1983@gmail.com>
2020-05-19 14:23:48 -0400

    I have made the value variable on the Coin class a read only variable.

--- SodaMachine/Coin.cs
+++ SodaMachine/Coin.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,18 @@ namespace SodaMachine
 {
     public abstract class Coin
     {
-        public double Value;
         double value;
+        public double Value
+        {
+            get
+            {
+                return value;
+            }
+        }
         public string name;
 
+       
+       
+        
     }
 }
