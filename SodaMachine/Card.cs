3b246af897fcc9ef7170d842815469a28d691cb6
unknown <mstiff1983@gmail.com>
2020-05-19 13:22:17 -0400

    In the Wallet Class, I instantiated the Wallet and added  in mixed change to the wallet and the card

--- SodaMachine/Card.cs
+++ SodaMachine/Card.cs
@@ -10,5 +10,10 @@ namespace SodaMachine
     {
         public double AvailableFunds;
         double availableFunds;
+
+        public Card()
+        {
+            AvailableFunds = 200;   
+        }
     }
 }
--- SodaMachine/Wallet.cs
+++ SodaMachine/Wallet.cs
@@ -10,5 +10,32 @@ namespace SodaMachine
     {
         public List<Coin> coins;
         public Card card;
+
+        public Wallet()
+        {
+            coins = new List<Coin>();
+            for (int index = 0; index < 16; index++)
+            {
+                Quarter quarter = new Quarter();
+                coins.Add(quarter);
+            }
+            for (int index = 0; index < 10; index++) 
+            {
+                Dime dime = new Dime();
+                coins.Add(dime);
+            }
+            for (int index = 0; index < 10; index++)
+            {
+                Nickle nickle = new Nickle();
+                coins.Add(nickle);
+            }
+            for (int index = 0; index < 50; indexx++)
+            {
+                Penny penny = new Penny();
+                coins.Add(penny);
+            }
+
+            card = new Card();
+        }   
     }
 }
